9315220f8eccac0477e10acd4b78cfdafcbe5cd2
amir.choubani <amirensit@gmail.com>
2020-01-19 11:00:30 +0100

    using env variable in a step for github actions

--- .github/scripts/decrypt_secret.sh
+++ .github/scripts/decrypt_secret.sh
@@ -2,6 +2,5 @@
 # Decrypt the file
 mkdir $HOME/secrets
 # --batch to prevent interactive command --yes to assume "yes" for questions
-# gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
-# --output $HOME/secrets/my_secret.json my_secrets.json.gpg
-ls
+gpg --quiet --batch --yes --decrypt --passphrase="$LARGE_SECRET_PASSPHRASE" \
+--output $HOME/secrets/my_secret.json ./.github/scripts/my_secrets.json.gpg
