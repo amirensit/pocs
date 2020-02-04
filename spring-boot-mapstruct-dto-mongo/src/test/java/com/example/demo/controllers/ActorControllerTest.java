afed507d706ae276d266d308c3775a6113410f59
achoubani <amir.choubani@almerys.com>
2020-02-04 08:58:25 +0100

    fixing spelling errors :p

--- spring-boot-mapstruct-dto-mongo/src/test/java/com/example/demo/controllers/ActorControllerTest.java
+++ spring-boot-mapstruct-dto-mongo/src/test/java/com/example/demo/controllers/ActorControllerTest.java
@@ -1,22 +1,16 @@
-package com.example.demo.Controllers;
+package com.example.demo.controllers;
 
-import com.example.demo.controllers.ActorController;
-import com.example.demo.controllers.errors.ErrorHandlingControllerAdvice;
 import com.example.demo.domain.Actor;
-import com.example.demo.services.ActorService;
 import com.example.demo.services.dto.ActorDTO;
 import com.example.demo.services.mapper.ActorMapper;
 import com.example.demo.services.mapper.ActorMapperTest;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MockMvcBuilder;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
