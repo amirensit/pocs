7e61da3ca2581c31c11a941b291f3dc6bbcc706a
amir.choubani <amirensit@gmail.com>
2020-02-23 13:46:40 +0100

    working on youssfi keycloak

---
+++ mohamed-youssfi-pocs-courses/keycloak/ecom-app/src/main/java/org/amirensit/pocs/ecomapp/EcomAppApplication.java
@@ -0,0 +1,13 @@
+package org.amirensit.pocs.ecomapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomAppApplication.class, args);
+	}
+
+}
---
+++ mohamed-youssfi-pocs-courses/keycloak/ecom-app/src/test/java/org/amirensit/pocs/ecomapp/EcomAppApplicationTests.java
@@ -0,0 +1,13 @@
+package org.amirensit.pocs.ecomapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
