0c41e86101b487fb5666482d982fdc3b750c0130
choubaniAmir <amirensit@gmail.com>
2020-02-29 15:21:33 +0100

    keycloak part 5 youssfi

---
+++ mohamed-youssfi-pocs-courses/keycloak/ecom-app/src/main/java/org/amirensit/pocs/ecomapp/config/KeycloakConfig.java
@@ -0,0 +1,14 @@
+package org.amirensit.pocs.ecomapp.config;
+
+import org.keycloak.adapters.springboot.KeycloakSpringBootConfigResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class KeycloakConfig {
+
+    @Bean
+    public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver () {
+        return new KeycloakSpringBootConfigResolver();
+    }
+}
