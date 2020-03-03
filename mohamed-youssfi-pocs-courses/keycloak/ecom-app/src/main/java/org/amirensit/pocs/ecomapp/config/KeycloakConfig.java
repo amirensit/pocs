152dbfd7ab08aab18e6a7c6bb483b8f6b0501bae
achoubani <amir.choubani@almerys.com>
2020-03-03 17:50:33 +0100

    keycloak youssfi part 5 min 29:49

--- mohamed-youssfi-pocs-courses/keycloak/ecom-app/src/main/java/org/amirensit/pocs/ecomapp/config/KeycloakConfig.java
+++ mohamed-youssfi-pocs-courses/keycloak/ecom-app/src/main/java/org/amirensit/pocs/ecomapp/config/KeycloakConfig.java
@@ -7,6 +7,9 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class KeycloakConfig {
 
+    /**
+     * Load Keycloak configuration from application.properties or application.yml, rather than keycloak.json.
+     */
     @Bean
     public KeycloakSpringBootConfigResolver keycloakSpringBootConfigResolver () {
         return new KeycloakSpringBootConfigResolver();
---
+++ mohamed-youssfi-pocs-courses/keycloak/ecom-app/src/main/java/org/amirensit/pocs/ecomapp/config/KeycloakSpringSecurityConfig.java
@@ -0,0 +1,39 @@
+
+package org.amirensit.pocs.ecomapp.config;
+
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+
+/**
+ * @author achoubani on 03/03/2020.
+ */
+@KeycloakConfiguration
+public class KeycloakSpringSecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
+
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+    /*
+    this method tells spring from where to find users and roles.
+    Is it from database or in-memory ect ect.
+    Here we are telling spring security to use the keyclakAuthenticationProvider.
+    Keycloak will be responsible for providing authentication services.
+     */
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests().antMatchers("/products/**").authenticated();
+    }
+}
