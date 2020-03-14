a41b7f41e7bc1cf7d99be8f38bca28666b685b29
amirensit <amirensit@gmail.com>
2020-03-14 13:57:18 +0100

    from ubunto part 8 youssfi keycloak min 20:00

--- mohamed-youssfi-pocs-courses/keycloak/angularFrontApp/src/app/services/keycloak-security.service.ts
+++ mohamed-youssfi-pocs-courses/keycloak/angularFrontApp/src/app/services/keycloak-security.service.ts
@@ -1,14 +1,22 @@
 import { Injectable } from '@angular/core';
 import { KeycloakInstance } from 'keycloak-js';
 
+declare let keycloak: any; // we need to use this line due to javascript code compilation error.
+
 @Injectable({
   providedIn: 'root'
 })
 export class KeycloakSecurityService {
 
+  public kc: KeycloakInstance; // public scope in order to be used anywhere in the app.
   constructor() { }
 
   init() {
     console.log('Security Initialisation');
+    this.kc = new keycloak({
+      url: "http://localhost:8080/auth",
+      realm: "ecom-realm",
+      client_id: "AngularProductsApp"
+    });
   }
 }
