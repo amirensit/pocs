27223874db97bbfc61fc403b3ed271e3d8015ccc
amirensit <amirensit@gmail.com>
2021-03-12 15:49:06 +0100

    youssfi kafka part 3 don

---
+++ mohamed-youssfi-pocs-courses/kafka-app/src/main/java/ConsumerApp.java
@@ -0,0 +1,36 @@
+
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.common.serialization.IntegerDeserializer;
+import org.apache.kafka.common.serialization.StringDeserializer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
+public class ConsumerApp {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        properties.put(ConsumerConfig.AUTO_COMMIT_INTERVAL_MS_CONFIG, "1000");
+        properties.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "true");
+        properties.put(ConsumerConfig.GROUP_ID_CONFIG, "test-group-1");
+        properties.put(ConsumerConfig.SESSION_TIMEOUT_MS_CONFIG, 30000);
+        properties.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        properties.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class.getName());
+        KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
+        kafkaConsumer.subscribe(Collections.singletonList("test-1")); // name of topics to subscribe
+        Executors.newScheduledThreadPool(1).scheduleAtFixedRate(() -> {
+            System.out.println("----------");
+            ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(10)); // vitesse of consumption: read messages during the last 100 ms
+            consumerRecords.forEach(consumerRecord -> {
+                System.out.println("key: " + consumerRecord.key());
+                System.out.println("value: " + consumerRecord.value());
+                System.out.println("offset: " + consumerRecord.offset());
+            });
+        }, 1000, 1000, TimeUnit.MILLISECONDS);
+    }
+}
