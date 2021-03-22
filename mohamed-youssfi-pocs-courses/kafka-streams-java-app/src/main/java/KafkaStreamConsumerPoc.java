5fe908e0bea188db62a6a0e02b3566cf40958be3
amirensit <amirensit@gmail.com>
2021-03-22 12:01:51 +0100

    youssfi kafka streams part 3 min 17:58

--- mohamed-youssfi-pocs-courses/kafka-streams-java-app/src/main/java/KafkaStreamConsumerPoc.java
+++ mohamed-youssfi-pocs-courses/kafka-streams-java-app/src/main/java/KafkaStreamConsumerPoc.java
@@ -1,12 +1,11 @@
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.Serdes;
-import org.apache.kafka.streams.KafkaStreams;
-import org.apache.kafka.streams.StreamsBuilder;
-import org.apache.kafka.streams.StreamsConfig;
-import org.apache.kafka.streams.Topology;
-import org.apache.kafka.streams.kstream.Consumed;
-import org.apache.kafka.streams.kstream.KStream;
+import org.apache.kafka.streams.*;
+import org.apache.kafka.streams.kstream.*;
 
+import java.time.Duration;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Arrays;
 import java.util.Properties;
 
@@ -29,8 +28,18 @@ public class KafkaStreamConsumerPoc {
             System.out.println("key: " + k);
             System.out.println("value: " + v);
         });*/
-        kStream.flatMapValues(value -> Arrays.asList(value.split("\\W+")))
-        .foreach((key, value) -> System.out.println(value));
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd").withZone(ZoneId.systemDefault());
+        KTable<Windowed<String>, Long> resultKTable = kStream.flatMapValues(value -> Arrays.asList(value.split("\\W+")))
+                .map((k, v) -> new KeyValue<>(k, v.toLowerCase()))
+                .filter((k, v) -> v.equals("a") || v.equals("b"))
+                .groupBy((k, v) -> v)
+                .windowedBy(TimeWindows.of(Duration.ofSeconds(5))) // statistics of only the last 5 seconds
+                .count(Materialized.as("count-analytics")); // give a name to that kTable to be used next
+
+        resultKTable.toStream()
+                .map((k, v) -> new KeyValue<>(formatter.format(k.window().startTime()) + "-" + formatter.format(k.window().endTime()) + "-" + k.key(), v))
+                .to("result-topic", Produced.with(Serdes.String(), Serdes.Long()));
+
 
         // start kafka streams
         Topology topology = streamsBuilder.build();
