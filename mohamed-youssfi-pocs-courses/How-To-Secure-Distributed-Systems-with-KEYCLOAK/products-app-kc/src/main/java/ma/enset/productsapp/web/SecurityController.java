38075c14037c588ff4a55f329b036f83e3a05f57
amirensit <amirensit@gmail.com>
2021-03-24 12:14:48 +0100

    youssfi keycloak part 2 min 1:11:43

---
+++ mohamed-youssfi-pocs-courses/How-To-Secure-Distributed-Systems-with-KEYCLOAK/products-app-kc/src/main/java/ma/enset/productsapp/web/SecurityController.java
@@ -0,0 +1,17 @@
+package ma.enset.productsapp.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class SecurityController {
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request) throws ServletException {
+        request.logout();
+        return "redirect:/";
+    }
+}
