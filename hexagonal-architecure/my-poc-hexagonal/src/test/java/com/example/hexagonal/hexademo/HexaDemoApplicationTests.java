3810f3874ad586f83b202e76dc1bfe05a47e9be2
amir_choubani <amir_choubani@carrefour.com>
2023-04-16 15:22:49 +0200

    hexagonal pocs

---
+++ hexagonal-architecure/my-poc-hexagonal/src/test/java/com/example/hexagonal/hexademo/HexaDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hexagonal.hexademo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexaDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
