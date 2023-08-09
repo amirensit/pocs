77cb1f02cf65abfc3f87fcafe0721901f1427972
amir_choubani <amir_choubani@carrefour.com>
2023-08-09 16:10:33 +0200

    openapi-generator-server-poc

---
+++ openapi-generator-server-poc/src/main/java/com/amirensit/poc/OpenapiGeneratorServerPocApplication.java
@@ -0,0 +1,13 @@
+package com.amirensit.poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenapiGeneratorServerPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OpenapiGeneratorServerPocApplication.class, args);
+	}
+
+}
---
+++ openapi-generator-server-poc/src/test/java/com/amirensit/poc/OpenapiGeneratorServerPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amirensit.poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenapiGeneratorServerPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
